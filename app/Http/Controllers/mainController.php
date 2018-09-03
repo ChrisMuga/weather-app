35eaea04f5d88ce92c7581be2ffd7cea9985665d
chris <chrismuga94@gmail.com>
2018-09-03 13:34:04 +0300

    appropriate the associative array

--- app/Http/Controllers/mainController.php
+++ app/Http/Controllers/mainController.php
@@ -9,15 +9,37 @@ class mainController extends BaseController
     #function1
     public function display()
     {
-        $apikey =   'bec78ab9a765701d40fc9d0b9bc1aace';
+        $apikey     =   'bec78ab9a765701d40fc9d0b9bc1aace';
+        $counter    =   0;
+        $stack      =   array();
         
-        $json   =   file_get_contents('http://api.openweathermap.org/data/2.5/group?id=184745,186301,191245&units=metric&APPID=bec78ab9a765701d40fc9d0b9bc1aace');
-        $data   =   json_decode($json, true);
+        $json       =   file_get_contents('http://api.openweathermap.org/data/2.5/group?id=184745,186301,191245&units=metric&APPID=bec78ab9a765701d40fc9d0b9bc1aace');
+        $data       =   json_decode($json, true);
 
-        #city-name
-        $cityName   =   $data["list"][0]["name"];
-        echo $cityName;
-        echo "<hr/>";
-        //return $data["list"][0];
+
+        #count
+
+        $count      =   $data["cnt"];
+
+
+        while($counter < $count)
+        {
+            #city-name
+            $cityName   =   $data["list"][$counter]["name"];
+            #temperature
+            $cityTemp   =   $data["list"][$counter]["main"]["temp"];
+
+            $stack[$counter]["city"]    =   $cityName;
+            $stack[$counter]["temp"]    =   $cityTemp;
+
+
+            #increment
+            $counter++;
+
+        }
+
+        return $stack;
+
+        #return $data["list"][0];
     }
 }
